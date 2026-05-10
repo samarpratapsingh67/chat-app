d568bea5f10096ee0516bc03956b6b2ab2fe16e0
samarpratapsingh67 <pratapsamar821@gmail.com>
2026-05-10 08:20:55 +0530

    updated version

--- proxy.js
+++ proxy.js
@@ -1,8 +1,13 @@
-import { clerkMiddleware ,createRouteMatcher} from '@clerk/nextjs/server';
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/api/create'])
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/create']);
+
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect()
-})
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
